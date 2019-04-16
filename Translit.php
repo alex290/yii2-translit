cb0761efd3321f0b1762d4080c8febe8c298f08e
Alexandr <alex@webgoal.ru>
2019-04-16 13:23:17 +0300

    Update

--- Translit.php
+++ Translit.php
@@ -5,10 +5,22 @@ namespace alex290\translit;
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
-{
-    public function run()
-    {
-        return "Hello!";
+class Translit extends \yii\base\Widget {
+
+    public $sufix = null;
+    public $text = null;
+
+    public function run() {
+        
+        $rus = [
+            'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', "?", " ", ".", "(", ")", ",", "'", "’", "'", '"', "«", "»"
+        ];
+        $lat = [
+            'A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya', "", "-", "", "", "", "", "", "", "", "", "", ""
+        ];
+        debug($rus);
+        debug($lat);
+        return str_replace($rus, $lat, $this->text).$this->sufix;
     }
+
 }
